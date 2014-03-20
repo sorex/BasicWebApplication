ed674c7e423e958678b29e7b0d0189c9213d1ee0
sorex <sorex1984@gmail.com>
2014-03-20 17:36:42 +0800

    日常提交

--- J.Web/Controllers/SingleController.cs
+++ J.Web/Controllers/SingleController.cs
@@ -7,11 +7,34 @@ using System.Web.Mvc;
 
 namespace J.Web.Controllers
 {
-    public class SingleController : BaseController
-    {
-        //
-        // GET: /Single/
-        public ActionResult Index()
+	public class SingleController : BaseController
+	{
+		//
+		// GET: /Single/
+		public ActionResult Index()
+		{
+			return View();
+		}
+
+        public ActionResult Detail()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete()
         {
             return View();
         }
